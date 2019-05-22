79bfcb6abaa502e45c484cdb5185814e616bad22
PmaiWoW <PmaiWoW@Gmail.com>
2019-05-22 09:56:49 +0100

    Update ex31_poo Program file

--- ex31_poo/Program.cs
+++ ex31_poo/Program.cs
@@ -16,7 +16,7 @@ namespace ex31_poo
             // Instrução 2
             // Esta instrução é inválida, pois a classe GameObject com o 
             // modificador de herança abstract, impedindo-a de ser instanciada;
-            // GameObject go = new GameObject();
+            GameObject go = new GameObject();
 
             // Instrução 3
             // Esta instrução é válida, pois, tal como a instrução 1, o
@@ -27,33 +27,33 @@ namespace ex31_poo
             // Instrução 4
             // Esta instrução é inválida, pois não é possível instanciar uma 
             // interface;
-            // ITaggable it = new ITaggable();
+            ITaggable it = new ITaggable();
 
             // Instrução 5
             // Instrução 5: Esta instrução é inválida, pois a classe 
             // GameObject não possui o método `DisableTrap()`, apenas a  classe
             // Trap;
-            // go.DisableTrap(); // go é uma variável do tipo GameObject
+            go.DisableTrap(); // go é uma variável do tipo GameObject
 
             // Instrução 6
             // Esta instrução é inválida, pois, tal como a instrução 2, a 
             // declaração da classe `GameObject` tem o modificador de herança 
             // `abstract`, impedindo-a de ser instanciada;
-            // PowerUp armorPacko = new GameObject();
+            PowerUp armorPacko = new GameObject();
 
             // Instrução 7
             // Esta instrução é inválida, pois tenta converter o método
             // DisableTrap() da classe Trap para tipo inteiro, o que é 
             // impossível;
             Trap gobj = new Trap();
-            //int p = gobj.DisableTrap; // gobj é do tipo Trap
+            int p = gobj.DisableTrap; // gobj é do tipo Trap
 
             //Instrução 8
             // Esta instrução é inválida, pois a interface ITaggable não é
             // implementada na classe Trap, logo uma instância desta classe não
             // pode ter uma tag como variável;
             Trap trap = new Trap();
-            // Console.WriteLine($"{trap.Tag}"); // trap é uma instância de Trap
+            Console.WriteLine($"{trap.Tag}"); // trap é uma instância de Trap
 
             // Instrução 9
             // Esta instrução é válida, pois a classe Trap é uma subclasse da
@@ -71,10 +71,12 @@ namespace ex31_poo
             GameObject go3 = new PowerUp();
 
             // Instrução 11
-            // 
+            // Esta instrução é válida, pois a classe PowerUp tem o método
+            // Priority, herdado da superclasse GameObject e sobreposto
+            // para retornar o valor do comprimento da Tag, podendo usar a Tag
+            // pois implementa a interface ITaggable;
             PowerUp pup = new PowerUp();
             Console.WriteLine($"{pup.Priority()}");// pup: instância de PowerUp
-
 
             Console.Write(armorPack.ToString());
 
