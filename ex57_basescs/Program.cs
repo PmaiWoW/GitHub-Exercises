ba0d8cdee43a623d7d4c91ce2ca2af743263ead9
PmaiWoW <PmaiWoW@Gmail.com>
2019-05-27 16:09:26 +0100

    Add test code for ex57 of basescs

---
+++ ex57_basescs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ex57_basescs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Process()
+            {
+                char[] a;
+                a = new char[] { 'a', 'b', 'c' };
+                MisteryOp(ref a);
+                return a;
+            }
+
+            void MisteryOp(ref char[] a)
+            {
+                a = new char[] { a[2], a[1], a[0] };
+            }
+
+            char[] chars = new char[3];
+
+            chars = Process();
+
+            foreach(char c in chars)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
